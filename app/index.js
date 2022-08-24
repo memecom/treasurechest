bb41c6253f2d6ed6e890c6177c1ffc59346e95d0
nishantrpai <nishantrpai@gmail.com>
2022-08-24 17:46:30 +0530

    should be gte to allowed

--- app/index.js
+++ app/index.js
@@ -158,7 +158,7 @@ var app = new Vue({
           this.user.account,
           this.vault.address
         )
-      ).isZero();
+      ).gte(ethers.BigNumber.from(allowed));
       this.unlockstates.approve = !needsallowance;
       this.unlockstates.increaseAllowance = !needsallowance;
 
